9b12e02ea001a8a3156e3ef3fe89466ff085116a
gholmesss <g.holmes429@gmail.com>
2021-03-14 15:18:06 -0500

    basic driver template

--- __main__.py
+++ __main__.py
@@ -1,3 +1,19 @@
 """
 
-"""
+"""#!/usr/bin/env python3
+"""Entry point to program."""
+
+from driver import Driver
+
+__author__ = "Grant Holmes"
+__email__ = "g.holmes429@gmail.com"
+
+
+def main():
+    """Main func."""
+    driver = Driver()
+    driver.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ driver.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+class Driver:
+
+	def __init__(self):
+		pass
+		
+	def run(self):
+		pass
