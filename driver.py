e0ed435d7fec165b92e7c5ab0281fab19781cbec
gholmes829 <g.holmes429@gmail.com>
2021-03-14 16:29:23 -0500

    image loading validation

--- driver.py
+++ driver.py
@@ -17,18 +17,18 @@ class Driver:
 	def __init__(self):
 		self.cwd = os.getcwd()
 		self.image_paths = os.path.join(self.cwd, "images")
+
+		# Loading the model vgg19 that will serve as the base model
+		self.model = models.vgg19(pretrained=True).features
+		# Assigning the GPU to the variable device
+		self.device = torch.device( "cuda" if (torch.cuda.is_available()) else 'cpu')
 		
 	def run(self):
-		# Loading the model vgg19 that will serve as the base model
-		model = models.vgg19(pretrained=True).features
-		# Assigning the GPU to the variable device
-		device = torch.device( "cuda" if (torch.cude.is_available()) else 'cpu')
+
 
 		# Loading the original and the style image
-		original_image = image_loader('jayhawk.jpg')
-		style_image = image_loader('style1.jpg')
-
-		self.show_image(original_image)
+		original_image = self.image_loader('jayhawk.jpg')
+		style_image = self.image_loader('style1.jpg')
 
 		#Creating the generated image from the original image
 		generated_image = original_image.clone().requires_grad_(True)
@@ -36,11 +36,15 @@ class Driver:
 	def image_loader(self, image_name):
 		image_path = os.path.join(self.image_paths, image_name)
 		image = Image.open(image_path)
+
+		#self.show_image(image)  # render image, blocking
+
 		# defining the image transformation steps to be performed before feeding them to the model
+
 		loader = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor()])
 		# The preprocessing steps involves resizing the image and then converting it to a tensor
 		image = loader(image).unsqueeze(0)
-		return image.to(device, torch.float)
+		return image.to(self.device, torch.float)
 
 	def show_image(self, image):
 		image.show()
