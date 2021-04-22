6f1b39b1fb73d8e7c891d49c109fb240d630b4d2
Grant Holmes <60802511+gholmes829@users.noreply.github.com>
2021-04-22 17:31:43 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,10 @@
 
 """
 
-content_name = "landscape.jpg"
-style_name = "cotton_candy.png"
+content_name = "undefined"
+style_name = "undefined"
 
-content_weight = 7
-style_weight = 80
+content_weight = 7  # determines how much content will be prioritized
+style_weight = 80  # determines how much style will be prioritized
 
-max_size = 1080
+max_size = 1080  # caps max image size on largest dimensions, automatically scales larger images down
